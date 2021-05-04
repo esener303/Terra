233446ce2d79ec073a0590198fc4c9d250808fe0
Michał Demczyszak <30467629+esener303@users.noreply.github.com>
2021-05-04 16:45:21 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-hello-azure-tf"
-    storage_account_name = "sahelloazuretf"
+    storage_account_name = "sahelloazuretf21091983"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
